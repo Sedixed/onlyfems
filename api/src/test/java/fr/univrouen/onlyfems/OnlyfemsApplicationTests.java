ecb9a26d24e276cdbe2e527b698d5e30f632ad43
Mathieu VALLEE <mathieu.vallee7@gmail.com>
2023-11-10 13:57:07 +0100

    feat: Init Springboot app

---
+++ api/src/test/java/fr/univrouen/onlyfems/OnlyfemsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.onlyfems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyfemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
