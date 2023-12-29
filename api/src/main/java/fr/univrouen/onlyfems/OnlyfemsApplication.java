a03a48342fab7781d127dad7a906c25b2a1e3f13
Alexandre MERRE <alexandre.merre.pro@gmail.com>
2023-12-29 13:48:09 +0100

    feat : Commented war deployment | May be useful later

--- api/src/main/java/fr/univrouen/onlyfems/OnlyfemsApplication.java
+++ api/src/main/java/fr/univrouen/onlyfems/OnlyfemsApplication.java
@@ -1,12 +1,20 @@
 package fr.univrouen.onlyfems;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class OnlyfemsApplication {
+// public class OnlyfemsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlyfemsApplication.class, args);
     }
+
+    // @Override
+    // protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    //     return application.sources(OnlyfemsApplication.class);
+    // }
 }
